9c0398a2ac81046eb0988f491eb0cb543bb52709
David Acconcio <dacconcio@gmail.com>
2018-09-11 18:42:57 -0400

    separated routes file

--- server.js
+++ server.js
@@ -1,54 +1,7 @@
-const User = require('./db/models.js')
-
 const express = require('express');
 const app = new express();
-const path = require('path');
-const bodyparser = require('body-parser');
 
-app.use(bodyparser.json());
-
-app.use('/dist', express.static(path.join(__dirname, 'dist')));
-
-app.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
-});
-
-app.get('/users/api', (req, res, next) => {
-  User.findAll()
-    .then(users => res.send(users))
-    .catch(err => next(err));
-});
-
-app.delete('/users/api/:id', (req, res, next) => {
-  User.findById(req.params.id)
-    .then(user => user.destroy())
-    .then(() => res.sendStatus(200))
-    .catch(err => next(err));
-});
-
-app.post('/users/api', (req, res, next) => {
-  User.create({ name: req.body.name })
-    .then(user => {
-      user.ManagerId = req.body.managerId;
-      user.save();
-      return user;
-    })
-    .then(user => res.send(user))
-    .catch(err => next(err));
-});
-
-app.put('/users/api', (req, res, next) => {
-
-  User.findById(req.body.userId)
-    .then(user => {
-      user.name = req.body.newName;
-      user.ManagerId = req.body.newManagerId;
-      user.save();
-      return user;
-    })
-    .then(user => res.send(user))
-    .catch(err => next(err));
-});
+require('./db/routes.js')(app)
 
 const PORT = process.env.PORT || 3000;
 
